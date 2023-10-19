890552ba672bc227adc26125e54bd1b08aac3367
ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>
2023-10-19 23:22:54 +0300

    feat: add support cors policy

---
+++ HomeBudget.Accounting.Api/Middlewares/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace HomeBudget_Accounting_Api.Middlewares
+{
+    internal static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCorrelationId(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CorrelationIdMiddleware>();
+        }
+    }
+}
--- HomeBudget.Accounting.Api/Program.cs
+++ HomeBudget.Accounting.Api/Program.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 using HomeBudget_Accounting_Api.Extensions;
 
 var webAppBuilder = WebApplication.CreateBuilder(args);
 var services = webAppBuilder.Services;
 var environment = webAppBuilder.Environment;
+var configuration = webAppBuilder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
+    .Build();
 
 webAppBuilder.Services.AddControllers();
 
@@ -14,20 +19,20 @@ webAppBuilder.Services.AddEndpointsApiExplorer();
 webAppBuilder.Services.AddSwaggerGen();
 
 services.SetupSwaggerGen();
+var webApp = webAppBuilder.Build();
 
-var app = webAppBuilder.Build();
+webApp.SetUpBaseApplication(services, environment, configuration);
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (webApp.Environment.IsDevelopment())
 {
-    app.SetUpSwaggerUi();
 }
 
-app.UseAuthorization();
+webApp.UseAuthorization();
 
-app.MapControllers();
+webApp.MapControllers();
 
-app.Run();
+webApp.Run();
 
 // To add visibility for integration tests
 namespace HomeBudget.Accounting.Api
