5fabf7d478024bf2a1b11945bf842dbc658cee26
ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>
2023-10-19 16:45:35 +0300

    test: cover payment account Api with tests (mock)

---
+++ HomeBudget.Accounting.Domain/Models/CurrencyTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeBudget.Accounting.Domain.Models
+{
+    public enum CurrencyTypes
+    {
+        BYN = 70,
+        USD = 10,
+        RUB = 20,
+        PLN = 30,
+        EUR = 40,
+        UAN = 50,
+        TRY = 60,
+    }
+}
