e13458c227d8bdf183ed630f7deacdf541276fd8
Pavel Chyzhykau <allmantool@gmail.com>
2024-05-14 09:18:36 +0300

    hotfix: add-tag-to-log (#101)
    Co-authored-by: ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>

---
+++ HomeBudget.Accounting.Api/Models/Operations/Requests/UpdateTransferRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HomeBudget.Accounting.Api.Models.Operations.Requests
+{
+    public record UpdateTransferRequest
+    {
+        public Guid TransferOperationId { get; set; }
+        public Guid Sender { get; set; }
+        public Guid Recipient { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Multiplier { get; set; }
+        public DateOnly OperationAt { get; set; }
+    }
+}
---
+++ HomeBudget.Components.Operations/Models/UpdateTransferPayload.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HomeBudget.Components.Operations.Models
+{
+    public record UpdateTransferPayload
+    {
+        public Guid TransferOperationId { get; set; }
+        public Guid Sender { get; set; }
+        public Guid Recipient { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Multiplier { get; set; }
+        public DateOnly OperationAt { get; set; }
+    }
+}
