7519f28b71286e97d9a1a2a9cd966aa98e494f26
Pavel Chyzhykau <allmantool@gmail.com>
2024-05-23 23:44:42 +0300

    fix: add auto comment for a transfer operation (#121)
    * fix: add auto comment for a transfer operation
    
    * chore: self review
    
    ---------
    
    Co-authored-by: ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>

---
+++ HomeBudget.Components.Operations/Commands/Models/UpdateTransferCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using MediatR;
+
+using HomeBudget.Accounting.Domain.Models;
+
+namespace HomeBudget.Components.Operations.Commands.Models
+{
+    internal class UpdateTransferCommand(CrossAccountsTransferOperation crossAccountsTransferOperations)
+        : IRequest<Result<Guid>>
+    {
+        public Guid Key { get; } = crossAccountsTransferOperations.Key;
+
+        public IReadOnlyCollection<FinancialTransaction> PaymentOperations { get; } = crossAccountsTransferOperations.PaymentOperations;
+    }
+}
