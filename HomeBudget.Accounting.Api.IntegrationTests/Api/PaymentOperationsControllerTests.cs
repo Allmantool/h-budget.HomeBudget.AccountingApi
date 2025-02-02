2ce2b1ab29f11d9ca654952c3ff452bb0b24917d
ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>
2025-02-02 13:26:56 +0300

    chore: self review

--- HomeBudget.Accounting.Api.IntegrationTests/Api/PaymentOperationsControllerTests.cs
+++ HomeBudget.Accounting.Api.IntegrationTests/Api/PaymentOperationsControllerTests.cs
@@ -149,7 +149,7 @@ namespace HomeBudget.Accounting.Api.IntegrationTests.Api
             var postCreateRequest = new RestRequest($"/{Endpoints.PaymentOperations}/{paymentAccountId}", Method.Post)
                 .AddJsonBody(requestBody);
 
-            var createOperationResult = await _sut.RestHttpClient.ExecuteWithDelayAsync<Result<CreateOperationResponse>>(postCreateRequest);
+            var createOperationResult = await _sut.RestHttpClient.ExecuteWithDelayAsync<Result<CreateOperationResponse>>(postCreateRequest, executionDelayInMs: 2000);
 
             var newOperationId = createOperationResult.Data.Payload.PaymentOperationId;
 
@@ -157,7 +157,7 @@ namespace HomeBudget.Accounting.Api.IntegrationTests.Api
 
             var deleteOperationRequest = new RestRequest($"{ApiHost}/{paymentAccountId}/{newOperationId}", Method.Delete);
 
-            await _sut.RestHttpClient.ExecuteWithDelayAsync<Result<RemoveOperationResponse>>(deleteOperationRequest);
+            await _sut.RestHttpClient.ExecuteWithDelayAsync<Result<RemoveOperationResponse>>(deleteOperationRequest, executionDelayInMs: 2000);
 
             var balanceAfter = (await GetPaymentsAccountAsync(paymentAccountId)).Balance;
 
