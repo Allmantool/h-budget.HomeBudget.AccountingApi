c5a6a7334510ab3e8f684eb58222a4bb32d67e8f
ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>
2025-02-09 12:44:27 +0300

    tech: extend monitoring for pub / sub

--- HomeBudget.Core/Constants/EventMetadataKeys.cs
+++ HomeBudget.Core/Constants/EventMetadataKeys.cs
@@ -3,5 +3,9 @@
     public static class EventMetadataKeys
     {
         public static readonly string Version = nameof(Version);
+
+        public static readonly string FromMessage = nameof(FromMessage);
+
+        public static readonly string ExceptionDetails = nameof(ExceptionDetails);
     }
 }
