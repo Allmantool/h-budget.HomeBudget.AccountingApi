9fdcd01a01fe12b85e118b600abeb64e8a2b023a
ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>
2025-08-16 20:23:42 +0300

    tech: try to stabalize event store client

---
+++ HomeBudget.Components.Operations/Factories/PaymentOperationEventChannelFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Channels;
+
+using HomeBudget.Components.Operations.Models;
+using HomeBudget.Core.Options;
+
+namespace HomeBudget.Components.Operations.Factories
+{
+    internal static class PaymentOperationEventChannelFactory
+    {
+        public static Channel<PaymentOperationEvent> CreateChannel(EventStoreDbOptions opts)
+        {
+            var capacity = opts.ChannelCapacity > 0 ? opts.ChannelCapacity : 10000;
+            var boundedOptions = new BoundedChannelOptions(capacity)
+            {
+                SingleReader = true,
+                SingleWriter = false,
+                FullMode = BoundedChannelFullMode.Wait
+            };
+
+            return Channel.CreateBounded<PaymentOperationEvent>(boundedOptions);
+        }
+    }
+}
