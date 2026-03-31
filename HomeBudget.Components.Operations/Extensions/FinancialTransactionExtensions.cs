907fc5b46b41b8835e6737643e3c80e7ef7c6bb8
ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>
2026-03-31 10:00:47 +0300

    chore: refactor PaymentOperationsHistoryService

---
+++ HomeBudget.Components.Operations/Extensions/FinancialTransactionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using HomeBudget.Accounting.Domain.Enumerations;
+using HomeBudget.Accounting.Domain.Models;
+
+namespace HomeBudget.Components.Operations.Extensions
+{
+    internal static class FinancialTransactionExtensions
+    {
+        public static decimal CalculateIncrement(
+            this FinancialTransaction operation,
+            IReadOnlyDictionary<Guid, Category> categoryMap)
+        {
+            if (operation.CategoryId == Guid.Empty)
+            {
+                return operation.Amount;
+            }
+
+            if (!categoryMap.TryGetValue(operation.CategoryId, out var category))
+            {
+                category = new Category(CategoryTypes.Expense, ["with empty category"]);
+            }
+
+            return category.CategoryType == CategoryTypes.Income
+                ? Math.Abs(operation.Amount)
+                : -Math.Abs(operation.Amount);
+        }
+    }
+}
